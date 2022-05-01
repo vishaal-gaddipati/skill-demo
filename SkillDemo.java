8d1ae91d89e8989900a09dd2b99264d859528beb
vishaal-gaddipati <vgaddipati@ucsd.edu>
2022-04-30 17:42:07 -0700

    Add test files

--- SkillDemo.java
+++ SkillDemo.java
@@ -1,3 +1,5 @@
 public class SkillDemo {
-
+    public static int multiply(int a, int b) {
+        return a*b;
+    }
 }
---
+++ SkillDemoTest.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemoTest {
+    @Test
+    public void mutliply() {
+        assertEquals(3, SkillDemo.multiply(1, 2));
+    }
+}
